0b939bc4bdacd44fd02c110ff14170fa6dd04f94
gabrielsfdev <ferreirawdev@gmail.com>
2023-01-13 20:42:08 -0300

    projeto de api superHero concluido

---
+++ QualSeuSuperHeroi/src/script.js
@@ -0,0 +1,23 @@
+import './style.css';
+import Swal from 'sweetalert2';
+
+const heroImg = document.querySelector('#heroImg');
+const btn = document.querySelector('button');
+const heroName = document.querySelector('#heroName');
+
+btn.addEventListener('click', () => {
+  const numMax = 800;
+  const idNumber = Math.floor(Math.random() * numMax);
+  fetch(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${idNumber}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      heroName.innerHTML = data.name;
+      heroImg.src = `https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/${data.slug}.jpg`;
+    }).catch(() => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Houve uma falha na matrix',
+      });
+    });
+});
